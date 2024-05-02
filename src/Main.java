aa8a2e9c80f565f82d9bff0df55adb4f1816fe68
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-02 11:21:15 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+//       [ java first programme ]
+        System.out.println("Hello World");
+
+
+//        [ mathematics in java without asking for input = in this method(1) we get directly sum of two numbers
+//        without asking for input from user]
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+
+
+//        conditional statements
+//                1 if , else
+
+        Scanner sc = new Scanner(System.in) ;
+        int age = sc.nextInt();
+
+        if(age>18) {
+            System.out.println("adult");
+        } else {
+            System.out.println("not adult");
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    }
+
+
+
